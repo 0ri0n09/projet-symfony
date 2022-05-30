5691d85fca029d8fbb11e97317b46eca2ff86afd
Logan CARMINATI <logan.carminati@ynov.com>
2022-05-30 23:35:29 +0200

    push back 5

---
+++ src/Controller/ArticleController.php
@@ -0,0 +1,45 @@
+<?php
+
+namespace App\Controller;
+
+use App\Entity\Commentaire;
+use App\Form\CommentaireType;
+use App\Repository\ArticleRepository;
+use App\Repository\CommentaireRepository;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+#[Route('/article')]
+class ArticleController extends AbstractController
+{
+    #[Route('/', name: 'app_liste_article')]
+    public function index(ArticleRepository $articleRepository): Response
+    {
+        $article = $articleRepository->FindAll();
+
+
+        return $this->render('article/list.html.twig', [
+            "article" => $article,
+            'controller_name' => 'ListeArticleController',
+        ]);
+    }
+
+    #[Route('/{id}', name: 'acteur_detail')]
+    public function acteurDetail($id, ArticleRepository $articleRepository, CommentaireRepository $commentaireRepository)
+    {
+        $article = $articleRepository->find($id);
+
+        $commentaires = $commentaireRepository->findBy(array("id_article" => $id));
+
+        $commentaire = new Commentaire();
+
+        $form = $this->createForm(CommentaireType::class, $commentaire);
+
+        return $this->render('/article/detail.html.twig', [
+            "commentaire" => $commentaires,
+            "form" => $form->createView(),
+            "article" => $article
+        ]);
+    }
+}
--- src/Controller/CreateArticleController.php
+++ src/Controller/CreateArticleController.php
@@ -4,18 +4,33 @@ namespace App\Controller;
 
 use App\Entity\Article;
 use App\Form\ArticleCreateType;
-use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use DateTime;
+use Doctrine\Persistence\ManagerRegistry;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 
 class CreateArticleController extends AbstractController
 {
     #[Route('/Create_article', name: 'app_create_article')]
-    public function createArticle(): Response
+    public function createArticle(Request $request, ManagerRegistry $doctrine): Response
     {
         $article = new Article();
 
         $form = $this->createForm(ArticleCreateType::class, $article);
+
+        $form->handleRequest($request);
+
+        if($form->isSubmitted() && $form->isValid()) {
+            $article->setIdUser($this->getUser());
+            $article->setCreateTime(new DateTime());
+            $manager = $doctrine->getManager();
+            $manager->persist($article);
+            $manager->flush();
+
+            return $this->redirectToRoute('app_liste_article');
+        }
 
         return $this->render('create_article/index.html.twig', [
             'form' => $form->createView(),
