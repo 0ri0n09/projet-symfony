608e3e9bfa67d83ea757f9c7a9eb8c66d2b0e261
Logan CARMINATI <logan.carminati@ynov.com>
2022-05-30 16:30:38 +0200

    Back Contact

---
+++ src/Controller/ContactController.php
@@ -0,0 +1,37 @@
+<?php
+
+namespace App\Controller;
+
+use App\Form\ContactType;
+use App\Entity\MessageContact;
+use DateTime;
+use Doctrine\Persistence\ManagerRegistry;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+
+class ContactController extends AbstractController
+{
+    #[Route('/contact', name: 'app_contact')]
+    public function index(Request $request, ManagerRegistry $doctrine): Response
+    {
+        $contact = new MessageContact();
+
+        $form = $this->createForm(ContactType::class, $contact);
+
+        $form->handleRequest($request);
+
+        if($form->isSubmitted() && $form->isValid()) {
+            $contact->setCreateTime(new DateTime());
+            $manager = $doctrine->getManager();
+            $manager->persist($contact);
+            $manager->flush();
+        }
+
+        return $this->render('contact/index.html.twig', [
+            'form' => $form->createView(),
+            'controller_name' => 'ContactController'
+        ]);
+    }
+}
